6e8dda46568b7adbdd18b44982298a2bb60db6d2
Jii Eu <jione.eu@gmail.com>
2020-08-23 12:06:22 +0900

    header logog and language selector added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,49 @@
 <template>
-   <div class="home">
-        <h1>v-m-blog</h1>
-        <div class="sections">
-            <div v-for="(section, index) in Object.keys(entries)" :key="index" class="group">
-                <h2>{{section}}</h2>
-                <div class="section" v-for="entry in entries[section]" :key="entry.id">
-                    <div class="entry">
-                        <h3 @click="$router.push({name: entry.id})">
-                        {{entry.title}}
-                        <span class="subtitle">{{entry.date}}</span>
-                        </h3>
-                        <p>{{entry.description}}</p>
-                    </div>
-                </div>
-            </div>  
+  <div class="home">
+    <div class="sections">
+      <div v-for="(section, index) in Object.keys(entries)" :key="index" class="group">
+        <h2>{{section}}</h2>
+        <div class="section" v-for="entry in entries[section]" :key="entry.id">
+          <div class="entry">
+            <h3 @click="$router.push({name: entry.id, title: entry.title})">
+              {{entry.title}}
+              <span class="subtitle">{{entry.date}}</span>
+            </h3>
+            <p>{{entry.description}}</p>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import BLOGENTRIES from '../statics/data/blogs.json';
+import BLOGENTRIES from "../statics/data/blogs.json";
 
 export default {
-    name: 'home',
-    computed: {
-        entries() {
-        return BLOGENTRIES
-        }
-    }
-  }
+  name: "home",
+  computed: {
+    entries() {
+      return BLOGENTRIES;
+    },
+  },
+};
 </script>
+
 <style lang="scss" scoped>
+.center {
+  text-align: center;
+}
+img {
+  display: block;
+  margin: 0 auto;
+  width: 150px;
+}
+h2 {
+  color: #35495e;
+  text-transform: capitalize;
+  margin-bottom: 2rem;
+}
 h3 {
   color: #42b883;
   margin-bottom: 0;
@@ -40,22 +53,23 @@ h3 {
   }
   .subtitle {
     color: grey;
-    font-size: .98rem;
+    font-size: 0.98rem;
     float: right;
     font-weight: normal;
   }
 }
-    .sections {
-    max-width: 40vw;
-    margin: 0 auto;
-    margin-top: 4rem;
-    }
-
-    .section {
-    margin-bottom: 3rem;
-    }
-
-    .group {
-    margin-bottom: 4rem;
-    }
+p {
+  margin-top: 0.4rem;
+}
+.sections {
+  max-width: 40vw;
+  margin: 0 auto;
+  margin-top: 4rem;
+}
+.section {
+  margin-bottom: 3rem;
+}
+.group {
+  margin-bottom: 4rem;
+}
 </style>
