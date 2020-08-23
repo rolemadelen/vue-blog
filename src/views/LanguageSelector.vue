c998bd4a493c3731bdc9064310b229a6f8a9e7a5
rolemadelen <rolemadelen17@pm.me>
2020-08-23 12:06:22 +0900

    header logog and language selector added

---
+++ src/views/LanguageSelector.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img :src="image" :alt="country">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        country: String,
+    },
+    data() {
+        return {
+            image: `/src/assets/${this.country}.png`,
+        }
+    }
+}
+</script>
