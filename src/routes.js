c73ca5b691bd77fca25708b0cf3e266aba4f4698
Jii Eu <jione.eu@gmail.com>
2020-08-26 01:45:27 +0900

    routes added to each language page

--- src/routes.js
+++ src/routes.js
@@ -10,9 +10,18 @@ import AboutKor from './views/AboutKor.vue';
 import AboutJap from './views/AboutJap.vue';
 
 import Til from './views/TIL.vue';
+
 import Algorithm from './views/Algorithm.vue';
+import AlgorithmKor from './views/AlgorithmKor.vue';
+import AlgorithmJap from './views/AlgorithmJap.vue';
+
 import Ps from './views/PS.vue';
+import PsKor from './views/PSKor.vue';
+import PsJap from './views/PSJap.vue';
+
 import DataStructure from './views/DataStructure.vue';
+import DataStructureKor from './views/DataStructureKor.vue';
+import DataStructureJap from './views/DataStructureJap.vue';
 
 Vue.use(VueRouter);
 
@@ -34,19 +43,24 @@ const blogRoutes = Object.keys(BlogEntries).map(section => {
 });
 
 export default new VueRouter({
-    base: process.env.BASE_URL,
+    base: __dirname,
     routes: [
-        { path: '/', name: 'home', component: Home, alias: ['/public/', '/eng/'] },
-        { path: '/kor/', name: 'home-kor', component: HomeKor, alias: '/kor' },
-        { path: '/jap/', name: 'home-jap', component: HomeJap, alias: '/jap'},
-        { path: '/eng/about/', name: 'about', component: About },
-        { path: '/kor/about/', name: 'about-jap', component: AboutJap },
-        { path: '/jap/about/', name: 'about-kor', component: AboutKor
- },
-        { path: '/eng/til', name: 'til', component: Til },
-        { path: '/eng/algorithm', name: 'algorithm', component: Algorithm },
-        { path: '/eng/data-structure', name: 'data-structure', component: DataStructure },
-        { path: '/eng/problem-solving', name: 'ps', component: Ps },
+        { path: '/', name: 'home', component: Home, alias: ['/public/', '/home/eng-home/'] },
+        { path: '/home/-kor-home/', name: 'home-kor', component: HomeKor, alias: '/home/kor-home' },
+        { path: '/home/-jap-home/', name: 'home-jap', component: HomeJap, alias: '/home/jap-home'},
+        { path: '/eng/eng-about/', name: 'about', component: About },
+        { path: '/kor/kor-about/', name: 'about-kor', component: AboutKor },
+        { path: '/jap/jap-about/', name: 'about-jap', component: AboutJap },
+        { path: '/eng/eng-til', name: 'til', component: Til },
+        { path: '/eng/eng-algorithm', name: 'algorithm', component: Algorithm },
+        { path: '/kor/kor-algorithm', name: 'algorithm-kor', component: AlgorithmKor },
+        { path: '/jap/jap-algorithm', name: 'algorithm-jap', component: AlgorithmJap },
+        { path: '/eng/eng-data-structure', name: 'data-structure', component: DataStructure },
+        { path: '/kor/kor-data-structure', name: 'data-structure-kor', component: DataStructureKor },
+        { path: '/jap/jap-data-structure', name: 'data-structure-jap', component: DataStructureJap },
+        { path: '/eng/eng-problem-solving', name: 'ps', component: Ps },
+        { path: '/kor/kor-problem-solving', name: 'ps-kor', component: PsKor },
+        { path: '/jap/jap-problem-solving', name: 'ps-jap', component: PsJap },
         ...blogRoutes
     ],
 })
