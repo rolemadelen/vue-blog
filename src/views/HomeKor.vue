58b2f438426e6474800f35aa2b627f51a5ed4684
j2ieu <j2ieu@pm.me>
2020-08-26 00:05:31 +0900

    sections top margin removed

--- src/views/HomeKor.vue
+++ src/views/HomeKor.vue
@@ -29,6 +29,11 @@ export default {
       return BLOGENTRIES;
     },
   },
+  data() {
+    return {
+      sectionName: ''
+    }
+  },
 };
 </script>
 
@@ -76,9 +81,7 @@ p.description {
   margin: 0 auto;
   margin-top: 4rem;
 }
-.section {
-  margin-bottom: 3rem;
-}
+
 .group {
   margin-bottom: 4rem;
 }
