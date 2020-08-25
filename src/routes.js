fea18f446046bdec2c9ba2a750257a2bf85a21ff
Madelen Yoo <rolemadelen@tuta.io>
2020-08-25 20:08:56 +0900

    Each routes added on each language
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+
 import Home from './views/Home.vue';
+import HomeKor from './views/HomeKor.vue';
+import HomeJap from './views/HomeJap.vue';
+
 import About from './views/About.vue';
-import AboutKo from './views/AboutKo.vue';
-import AboutJa from './views/AboutJa.vue';
+import AboutKor from './views/AboutKor.vue';
+import AboutJap from './views/AboutJap.vue';
+
 import Til from './views/TIL.vue';
 import Algorithm from './views/Algorithm.vue';
 import Ps from './views/PS.vue';
@@ -17,9 +22,9 @@ const blogRoutes = Object.keys(BlogEntries).map(section => {
     const children = BlogEntries[section].map(child => ({
         path: child.id,
         name: child.id,
-        title: child.title,
-        component: () => import(`./posts/${section}/${child.id}.md`)
-    }))
+        lang: child.lang,
+        component: () => import(`./posts/${child.lang}/${section}/${child.id}.md`)
+    }));
     return {
         path: `/${section}`,
         name: section,
@@ -31,15 +36,17 @@ const blogRoutes = Object.keys(BlogEntries).map(section => {
 export default new VueRouter({
     base: process.env.BASE_URL,
     routes: [
-        { path: '/', name: 'home', component: Home },
-        { path: '/public/', name: 'public-home', component: Home },
-        { path: '/about/', name: 'about', component: About },
-        { path: '/about/ja/', name: 'about-ja', component: AboutJa },
-        { path: '/about/ko/', name: 'about-ko', component: AboutKo },
-        { path: '/til/', name: 'til', component: Til },
-        { path: '/algorithm/', name: 'algorithm', component: Algorithm },
-        { path: '/data-structure/', name: 'data-structure', component: DataStructure },
-        { path: '/problem-solving/', name: 'ps', component: Ps },
+        { path: '/', name: 'home', component: Home, alias: ['/public/', '/eng/'] },
+        { path: '/kor/', name: 'home-kor', component: HomeKor, alias: '/kor' },
+        { path: '/jap/', name: 'home-jap', component: HomeJap, alias: '/jap'},
+        { path: '/eng/about/', name: 'about', component: About },
+        { path: '/kor/about/', name: 'about-jap', component: AboutJap },
+        { path: '/jap/about/', name: 'about-kor', component: AboutKor
+ },
+        { path: '/eng/til', name: 'til', component: Til },
+        { path: '/eng/algorithm', name: 'algorithm', component: Algorithm },
+        { path: '/eng/data-structure', name: 'data-structure', component: DataStructure },
+        { path: '/eng/problem-solving', name: 'ps', component: Ps },
         ...blogRoutes
     ],
 })
