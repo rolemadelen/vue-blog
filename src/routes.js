7dd50680d61b1ac70e456fccc0f4ac6d66924c45
rolemadelen <rolemadelen17@pm.me>
2020-08-25 17:33:11 +0900

    route path added to each tab

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,12 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
+import AboutKo from './views/AboutKo.vue';
+import AboutJa from './views/AboutJa.vue';
+import Til from './views/TIL.vue';
+import Algorithm from './views/Algorithm.vue';
+import Ps from './views/PS.vue';
+import DataStructure from './views/DataStructure.vue';
 
 Vue.use(VueRouter);
 
@@ -28,6 +34,12 @@ export default new VueRouter({
         { path: '/', name: 'home', component: Home },
         { path: '/public/', name: 'public-home', component: Home },
         { path: '/about/', name: 'about', component: About },
+        { path: '/about/ja/', name: 'about-ja', component: AboutJa },
+        { path: '/about/ko/', name: 'about-ko', component: AboutKo },
+        { path: '/til/', name: 'til', component: Til },
+        { path: '/algorithm/', name: 'algorithm', component: Algorithm },
+        { path: '/data-structure/', name: 'data-structure', component: DataStructure },
+        { path: '/problem-solving/', name: 'ps', component: Ps },
         ...blogRoutes
     ],
 })
