c8a091a059aa72875893713bd6528822cf40ac17
Madelen Yoo <rolemadelen@tuta.io>
2020-08-26 22:04:17 +0900

    components separated
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- views/DataStructure.vue
+++ views/DataStructure.vue
@@ -1,39 +1,13 @@
 <template>
   <div class="home">
-    <div class="sections">
-      <div v-for="(section, index) in Object.keys(entries)" :key="index" class="group">
-        <div v-if="section === 'Data Structure'">
-          <h2>{{section}}</h2>
-          <div class="section" v-for="entry in entries[section]" :key="entry.id">
-            <div v-if="entry.lang === '/' ||entry.lang === 'eng'">
-              <div class="entry">
-                <h3 @click="$router.push({name: entry.id, title: entry.title})">
-                  {{entry.title}}
-                  <span class="date">{{entry.date}}</span>
-                </h3>
-                <p class="description">{{entry.description}}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <list-posts _section="Data Structure" lang="/eng"/>
   </div>
 </template>
 
 <script>
-import BLOGENTRIES from "../statics/data/blogs.json";
+import ListPosts from "./components/ListPosts.vue";
 
 export default {
-  name: "home",
-  computed: {
-    entries() {
-      return BLOGENTRIES;
-    },
-  },
+  components: { ListPosts },
 };
 </script>
-
-<style lang="scss" scoped>
-  @import "../styles/_home.scss";
-</style>
---
+++ views/components/HeaderMenu.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <router-link :to="page">{{this.name}}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    page: String,
+    name: String,
+  },
+};
+</script>
