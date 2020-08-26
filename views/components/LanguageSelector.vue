c103763802175fc1f5f47015a80730ef39386156
rolemadelen <rolemadelen17@pm.me>
2020-08-26 22:04:17 +0900

    components separated

---
+++ views/components/LanguageSelector.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img :src="image" :alt="lang" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    lang: String,
+  },
+  data() {
+    return {
+      image: `./assets/${this.lang}.png`,
+    };
+  },
+};
+</script>
