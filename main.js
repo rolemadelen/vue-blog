9874d91f221088153a234b5b43e10968b91ab6b2
Madelen Yoo <rolemadelen@tuta.io>
2020-08-27 02:55:56 +0900

    prism css/js added
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import routes from './routes';
-
+import './styles/prism.js';
+import './styles/prism.css';
 Vue.config.productionTip = false;
+// Vue.config.devtools = false;
 
 new Vue({
     router: routes,
