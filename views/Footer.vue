4910efa040b41aba5ba22667f6ccb3448fc86159
Madelen Yoo <rolemadelen@tuta.io>
2020-08-30 08:52:59 +0900

    footer updated
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- views/Footer.vue
+++ views/Footer.vue
@@ -8,14 +8,15 @@
     </div>
     <div class="sns center">
       <span class="github">
-        <a href="https://github.com/jioneeu">GitHub</a>
+        <a href="https://github.com/jioneeu" target="_blank">GitHub</a>
       </span> |
       <span class="twitter">
-        <a href="https://twitter.com/jioneeu">Twitter</a>
+        <a href="https://twitter.com/jioneeu" target="_blank">Twitter</a>
       </span> |
       <span class="youtube">
         <a
           href="https://www.youtube.com/channel/UC8hY3wjYlK2U9W4fqKN598Q?view_as=subscriber"
+          target="_blank"
         >YouTube</a>
       </span>
     </div>
@@ -26,7 +27,7 @@
 #footer {
   position: relative;
   width: 100%;
-  height: 6em;
+  height: 7em;
   border-top: 1px solid rgb(0, 0, 0, 0.1);
   background-color: rgba(255, 255, 255, 1);
   color: rgba(0, 0, 0, 0.6);
@@ -52,36 +53,37 @@
 
   .created-by {
     font-size: 12px;
-    padding-bottom: 35px;
+    padding-bottom: 47px;
   }
 
   .powered-by {
     font-size: 11px;
     font-weight: 300;
-    margin-top: px;
+    padding-bottom: 10px;
   }
 
   .sns {
-      display: flex;
-      min-width: 20em;
-      justify-content: space-between;
+    display: flex;
+    min-width: 20em;
+    justify-content: space-between;
+
+    margin-top: 25px;
+    font-size: 11px;
 
     a {
       font-weight: 400;
       text-decoration: none;
     }
-    margin-top: 30px;
-    font-size: 11px;
 
     .github a:hover {
-        color: #000;
+      color: #000;
     }
     .twitter a:hover {
       color: rgb(48, 168, 255);
     }
 
     .youtube a:hover {
-        color: rgb(189, 66, 89);
+      color: rgb(189, 66, 89);
     }
   }
 }
