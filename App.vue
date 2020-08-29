7081ee92801bc4582138ac48a5dec950c5a8a07b
Madelen Yoo <rolemadelen@tuta.io>
2020-08-30 03:24:24 +0900

    font family changed
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- App.vue
+++ App.vue
@@ -16,6 +16,10 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+}
+
 ::selection {
     background: rgba(225, 225, 243, 0.2);
     text-shadow: none;
