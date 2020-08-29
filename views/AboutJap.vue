4c0a727c73bb14aa207a1d520c80eab23522d92f
Madelen Yoo <rolemadelen@tuta.io>
2020-08-30 04:14:31 +0900

    About page wording changed
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- views/AboutJap.vue
+++ views/AboutJap.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="about">
     <h2 style="text-align: center;">こんにちは！</h2>
-    <b>jioneeu</b>という名でネットで活動している<b>Jione Eu</b>です。
+    ネットで<b>jioneeu</b>という名で活動している<b>Jii Eu</b>と申します。
     <br /><br />
     
-    2019年<a href="https://ucsd.edu/" target="_blank">UC San Diego</a>から<a href="http://cogsci.ucsd.edu/undergraduates/major/design-interaction.html" target="_blank">認知科学 HCI</a>学位を受けて卒業しました。
+    韓国生まれですが、小３ぐらいからアメリカで育ちました。大学は2019年<a href="https://ucsd.edu/" target="_blank">UC San Diego</a>から<a href="http://cogsci.ucsd.edu/undergraduates/major/design-interaction.html" target="_blank">認知科学 HCI</a>学位を受けて卒業しました。
+    
     <br /><br />
     
-    韓国生まれで、アメリカ育ちです。今は日本の小学校で英語の先生として働いていますが、software developerを目指して一生懸命勉強しています。
+    今は日本の小学校で英語の先生として働いていますが、web developerを目指して一生懸命勉強しています。
+    
     <br /><br />
 
-    また自分の動機付けのために
-    <a href="https://www.youtube.com/channel/UC8hY3wjYlK2U9W4fqKN598Q?view_as=subscriber" target="_blank">Study with me</a>チャンネルを運営しています。開発の勉強ではなくても大丈夫なので、一緒に勉強したいと思う方はいつでも見に来てください。
+    また自分の動機付けのために<a href="https://www.youtube.com/channel/UC8hY3wjYlK2U9W4fqKN598Q?view_as=subscriber" target="_blank">Study with me</a>チャンネルを運営しています。一緒に勉強したいと思う方はいつでも来てください。
+
     <br /><br />
 
     作成した記事の内容に誤字脱字及び文脈が変な部分がありましたら是非、 私の<a href="https://twitter.com/jioneeu" target="_blank">Twitter</a>にメッセージをしてください。
