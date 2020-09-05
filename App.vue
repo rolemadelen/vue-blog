3b092b577b8660b6033d092ec8e44050ee0fbba4
Madelen Yoo <rolemadelen@tuta.io>
2020-09-05 13:59:30 +0900

    mobile width modified & eng-queue implementation code changed
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- App.vue
+++ App.vue
@@ -18,13 +18,37 @@ export default {
 </script>
 
 <style lang="scss">
-body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-}
+@media only screen and (min-width: 1025px) {
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
 
-::selection {
+  ::selection {
     background: rgba(225, 225, 243, 0.2);
     text-shadow: none;
+  }
+}
+
+@media only screen and (min-width: 320px) and (max-width: 480px) {
+  body {
+    margin: 0 1.2em;
+    padding: 0 5px;
+
+    .sections {
+      font-size: 13px;
+
+      h3 {
+        font-size: 14px;
+      }
+
+      p.description {
+      // これ嫌だな
+        width: 100% !important;
+      }
+    }
+  }
 }
 </style>
