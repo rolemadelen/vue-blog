2da3c771ffb23b28e35ede2d7c6f10314d087bf0
Madelen Yoo <rolemadelen@tuta.io>
2020-10-24 20:36:45 +0900

    About page updated & Content width widened
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- views/AboutJap.vue
+++ views/AboutJap.vue
@@ -1,29 +1,52 @@
 <template>
   <div class="about">
-    <h2 style="text-align: center;">こんにちは！</h2>
-    ネットで<b>jioneeu</b>という名で活動している<b>Jii Eu</b>と申します。
-    <br /><br />
-    
-    韓国生まれですが、小３ぐらいからアメリカで育ちました。大学は2019年<a href="https://ucsd.edu/" target="_blank">UC San Diego</a>から<a href="http://cogsci.ucsd.edu/undergraduates/major/design-interaction.html" target="_blank">認知科学 HCI</a>学位を受けて卒業しました。
-    
-    <br /><br />
-    
-    今は日本の小学校で英語の先生として働いていますが、web developerを目指して一生懸命勉強しています。
-    
-    <br /><br />
+    <div id="bug-title">ユウバグです。</div>
 
-    また自分の動機付けのために<a href="https://www.youtube.com/channel/UC8hY3wjYlK2U9W4fqKN598Q?view_as=subscriber" target="_blank">Study with me</a>チャンネルを運営しています。一緒に勉強したいと思う方はいつでも来てください。
+    <div id="bug-content">
+      日本の小学校で英語を教えています。<br>今はフロントエンド・エンジニアを目指してVue.jsとReactの勉強をしています。
 
-    <br /><br />
+      <br /><br />
 
-    作成した記事の内容に誤字脱字及び文脈が変な部分がありましたら是非、 私の<a href="https://twitter.com/jioneeu" target="_blank">Twitter</a>にメッセージをしてください。
-    <br /><br />
-    
-    よろしくお願いします。
+    <div class="bug-links">
+      <div><a href="https://github.com/bugxvii">GitHub</a></div>
+      <a href="https://www.instagram.com/jioneeu_4am/">Instagram</a>
+      <div><a href="https://www.linkedin.com/in/jioneeu/">LinkedIn</a></div> 
+    </div>
+    <div class="bug-links">
+      <a href="https://twitter.com/bugxvii">Twitter</a>
+      <div><a href="https://www.youtube.com/channel/UC8hY3wjYlK2U9W4fqKN598Q?view_as=subscriber">YouTube</a></div>
+    </div>
+      </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+#bug-title {
+  text-align: center;
+  font-family: "Maven Pro", "sans-serif";
+  margin-bottom: 25px;
+  font-size: 2.7em;
+}
+
+#bug-content {
+  margin: 0 auto;
+  max-width: 25em;
+}
+
+.bug-links {
+  display: flex;
+  justify-content: space-between;
+
+  a {
+    color: black;
+  }
+
+  a:hover {
+    color: #7a7b7c;
+  
+  }
+}
+
 .about {
   font-size: 15px;
   line-height: 1.5;
@@ -34,9 +57,16 @@
     margin-bottom: 2em;
   }
 
-  a {
-    text-decoration: none;
-    color: #176ac1;
+  .tgl-btn {
+    text-decoration: underline;
   }
+  .tgl-btn:hover {
+    cursor: pointer;
+  }
+}
+
+ul,
+li {
+  list-style-type: none;
 }
 </style>
